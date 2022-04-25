5f9e196023e9ae904001fd6fb019d4e82a32ea19
victor-malloni <oxemalloni@gmail.com>
2022-04-25 12:26:24 -0300

    criando logica dos spinners

--- app/src/main/java/com/example/conversor/MainActivity.kt
+++ app/src/main/java/com/example/conversor/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.conversor
 import android.net.Network
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 import com.example.conversor.API.EndPoint
 import com.example.conversor.util.NetworkUtils
 import com.google.gson.JsonObject
@@ -11,13 +12,27 @@ import retrofit2.Response
 import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var spFrom : Spinner
+    private lateinit var spTo : Spinner
+    private lateinit var btConvert : Button
+    private lateinit var tvResult : TextView
+    private lateinit var etValueFrom : EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         getCurrencies()
-    }
 
+
+        spFrom = findViewById(R.id.spFrom)
+        spTo = findViewById(R.id.spTo)
+        btConvert = findViewById(R.id.btConvert)
+        tvResult = findViewById(R.id.tvResult)
+        etValueFrom = findViewById(R.id.etValueFrom)
+
+    }
 
 
     fun getCurrencies() {
@@ -32,12 +47,28 @@ class MainActivity : AppCompatActivity() {
                     data.add(it)
                 }
 
-                println(data.count())
-            }
+                val moedaBRL = data.indexOf("brl")
+                val moedaUSD = data.indexOf("usd")
 
+                val adapter = ArrayAdapter(baseContext,android.R.layout.simple_spinner_dropdown_item, data)
+                spFrom.adapter = adapter
+                spTo.adapter = adapter
+
+                //setar valor inicial das moedas
+                spFrom.setSelection(moedaBRL)
+                spTo.setSelection(moedaUSD)
+            }
             override fun onFailure(call: Call<JsonObject>, t: Throwable) {
                 println("Error callback")
             }
+
+
+
         })
+
+
     }
+
+
+
 }
